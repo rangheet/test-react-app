a717b6096c56a5b8093e0eedec57580cafb2184f
rangheet <heetdave@outlook.com>
2019-03-31 11:29:44 +0530

    Updates

---
+++ precache-manifest.405ee3cf2007fe681288425b2c743714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3f757ad1c769bd8640d",
+    "url": "/test-react-app/static/js/runtime~main.66c072d8.js"
+  },
+  {
+    "revision": "627b09c2fac5d252675d",
+    "url": "/test-react-app/static/js/main.f53199d2.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/test-react-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "627b09c2fac5d252675d",
+    "url": "/test-react-app/static/css/main.238fa998.chunk.css"
+  },
+  {
+    "revision": "9f73f2df7694370726d099854c38e7c5",
+    "url": "/test-react-app/index.html"
+  }
+];
